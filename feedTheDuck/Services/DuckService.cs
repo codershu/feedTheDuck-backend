03eece51f80ad7f4672b31b4ea87393f1a8f784b
Shu Han <codershuhan@gmail.com>
2022-01-01 23:38:18 -0800

    1st api

---
+++ feedTheDuck/Services/DuckService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using feedTheDuck.Interfaces;
+using feedTheDuck.Models;
+
+namespace feedTheDuck.Services
+{
+    public class DuckService : IDuckService
+    {
+        public DuckService()
+        {
+        }
+
+        public Task<Response<bool>> AddRecord(DuckRecordRequest request)
+        {
+            return Task.FromResult(Response<bool>.Ok(true, "New Record Added"));
+        }
+    }
+}
